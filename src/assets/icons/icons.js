e0e5c2507e48f1e477b61637422184e5f27c07c6
MeryAmun <chebesiawah@gmail.com>
2023-06-23 12:20:31 +0100

    images and icons imported

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,13 @@
+import { FaPhone,FaTwitter,FaInstagram,FaWhatsapp,FaShoppingCart,FaTruckFast,FaUser,FaCalendarDay } from 'react-icons/fa'
+import { MdOutlineEmail,MdSupportAgent,MdAccessTimeFilled,MdLocationPin } from 'react-icons/md'
+import { GrFacebookOption } from 'react-icons/gr'
+import { CiDiscount1 } from 'react-icons/ci'
+import { HiMiniShieldCheck } from 'react-icons/hi'
+import { BsArrowRightShort } from 'react-icons/bs'
+
+
+
+
+export {
+    FaPhone,MdOutlineEmail,GrFacebookOption,FaTwitter,FaInstagram,FaWhatsapp,FaShoppingCart,FaTruckFast,CiDiscount1,MdSupportAgent,HiMiniShieldCheck,BsArrowRightShort,FaUser,MdAccessTimeFilled,FaCalendarDay,MdLocationPin
+}
